5b0e38d6c9adcbc44d6584d30c6f4bfd22a8c65a
Abdul Rehman <i192190@lhr.nu.edu.pk>
2024-04-19 14:52:39 +0500

    Update network.tf

--- network.tf
+++ network.tf
@@ -17,7 +17,7 @@ resource "azurerm_subnet" "my_terraform_subnet_1" {
 
 # Subnet 2
 resource "azurerm_subnet" "my_terraform_subnet_2" {
-  name                 = "subnet-2"
+  name                 = "subnet-2.0"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.my_terraform_network.name
   address_prefixes     = ["10.0.1.0/24"]
